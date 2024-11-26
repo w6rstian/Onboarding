4213ad6c0bda14692273499848aa22ecf533c573
Klaudia Klim <klaudia.n.klim@gmail.com>
2024-11-26 15:17:49 +0100

    Stworzenie strony powitalnej z opcjami dla każdego rodzaju pracownika, dodanie rejestracji nowego pracownika po przejściu w "Zacznij Teraz"

---
+++ Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Onboarding.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult CreateEmployee()
+        {
+            return View();
+        }
+    }
+}
