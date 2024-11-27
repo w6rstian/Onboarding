8be06c497b301396b88b789a23c4b671d653b656
w6rstian <quarennc@gmail.com>
2024-11-27 03:37:38 +0100

    Added example controller for register form data validation.

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Onboarding.Services;
+
+namespace Onboarding.Controllers
+{
+	/// <summary>
+	/// This controller is for example register form data validation.
+	/// </summary>
+	public class RegisterController : Controller
+	{
+		private ValidationService _validationService;
+
+		public RegisterController(ValidationService validationService)
+		{
+			_validationService = validationService;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Register(RegisterViewModel model)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(model);
+			}
+
+			if (!_validationService.ValidateLoginFormat(model.Login))
+			{
+				ModelState.AddModelError("Login", "Login must be alphanumeric and have 5-50 characters.");
+				return View(model);
+			}
+
+			if (!_validationService.ValidatePasswordFormat(model.Password))
+			{
+				ModelState.AddModelError("Password", "Password must be at least 8 characters long, including a letter, digit, and a special character.");
+				return View(model);
+			}
+
+			if (!_validationService.ValidateEmailFormat(model.Email))
+			{
+				ModelState.AddModelError("Email", "Incorrect email format.");
+				return View(model);
+				
+			}
+
+			var isLoginUnique = await _validationService.IsLoginUniqueAsync(model.Login);
+
+			if (!isLoginUnique)
+			{
+				ModelState.AddModelError("Login", "Login is alraedy taken.");
+				return View(model);
+			}
+
+			var isEmailUnique = await _validationService.IsEmailUniqueAsync(model.Email);
+			if (!isEmailUnique)
+			{
+				ModelState.AddModelError("Email", "Email is already taken.");
+				return View(model);
+			}
+
+			return RedirectToAction("Success");
+		}
+	}
+}
