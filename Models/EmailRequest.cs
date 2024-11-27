0bb2c087eb5d46c510fd83cf593339379954c80f
w6rstian <quarennc@gmail.com>
2024-11-27 02:58:16 +0100

    Added email request model, which holds recipient, subject, and email body.

---
+++ Models/EmailRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Onboarding.Models
+{
+	public class EmailRequest
+	{
+		public string RecipientEmail { get; set; }
+		public string Subject { get; set; }
+		public string MessageBody { get; set; }
+	}
+}
