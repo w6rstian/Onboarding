79f45577b0bb7f2cb4be20e533a5d7d960fa616e
w6rstian <quarennc@gmail.com>
2024-11-27 03:37:54 +0100

    Added example interfaces for register form data validation.

---
+++ Interfaces/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Onboarding.Interfaces
+{
+	/// <summary>
+	/// This interface is for example register form data validation.
+	/// </summary>
+	public interface IUserRepository
+	{
+		async Task<bool> UserExistsByLoginAsync(string login);
+		async Task<bool> UserExistsByEmailAsync(string email);
+	}
+}
---
+++ Interfaces/IValidationService.cs
@@ -0,0 +1,14 @@
+﻿namespace Onboarding.Interfaces
+{
+	/// <summary>
+	/// This interface is for example register form data validation.
+	/// </summary>
+	public interface IValidationService
+	{
+		bool ValidateLoginFormat(string login);
+		bool ValidatePasswordFormat(string password);
+		bool ValidateEmailFormat(string email);
+		async Task<bool> IsLoginUniqueAsync(string login);
+		async Task<bool> IsEmailUniqueAsync(string email);
+	}
+}
