fbe33cfaadd3a7627fcc0eeb583172e95b105dbc
Sebastian <86176@student.pb.edu.pl>
2025-04-01 14:23:23 +0200

    BuddyController is used for buddy-specific tasks, for example checking progress of buddy's subordinates.

---
+++ Controllers/BuddyController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Onboarding.Data;
+using Onboarding.Models;
+
+namespace Onboarding.Controllers
+{
+    public class BuddyController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<User> _userManager;
+
+        public BuddyController(ApplicationDbContext context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> TaskStatus()
+        {
+            var currentUser = await _userManager.GetUserAsync(User);
+
+            var tasks = _context.UserTasks
+                .Where(t => t.user.Buddy == currentUser)
+                .ToList();
+
+
+            // DEBUG
+            var nowi = await _userManager.GetUsersInRoleAsync("Nowy");
+            var nowy1 = nowi.FirstOrDefault(t => t.Email == "nowy1@mail.com");
+
+
+            var tempUserTask = new UserTask
+            {
+                user = nowy1,
+                Status = StatusTask.InProgress
+            };
+
+            if (currentUser == nowy1.Buddy)
+            {
+                tasks.Add(tempUserTask);
+            }
+
+            // DEBUG END
+
+            return View(tasks);
+        }
+    }
+}
