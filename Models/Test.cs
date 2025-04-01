5adf9bd34bd71c9cf6a88e85334700844a62dc66
Klaudia Klim <klaudia.n.klim@gmail.com>
2025-04-01 16:56:03 +0200

    Testy mają pytania i odpowiedzi zamiast tasków

--- Models/Test.cs
+++ Models/Test.cs
@@ -10,11 +10,12 @@ namespace Onboarding.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int? TaskId { get; set; }
 
-        public Task Task { get; set; }
+        public int CourseId { get; set; }
         public Course Course { get; set; }
 
-        public ICollection<Question> Questions { get; set; } = new List<Question>();
+        public List<Question> Questions { get; set; } = new List<Question>();
     }
+
 }
+
