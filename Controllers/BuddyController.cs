07cd30709aa830bb0d6533c1f945764a900a4d91
szymanowsk <szymanowskakam@gmail.com>
2025-04-15 13:20:34 +0200

    panel buddy'ego dostępny dla buddy'ego

--- Controllers/BuddyController.cs
+++ Controllers/BuddyController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Onboarding.Data;
@@ -7,6 +8,7 @@ using Onboarding.Models;
 
 namespace Onboarding.Controllers
 {
+    [Authorize(Roles = "Buddy")]
     public class BuddyController : Controller
     {
         private readonly ApplicationDbContext _context;
