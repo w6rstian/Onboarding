252d21aa85d66c9b8442dace70df5350a03e213c
Kamila Szymanowska <86179@student.pb.edu.pl>
2025-04-15 15:06:25 +0200

    gdy jest tworzone konto ma defaultowo rolę "nowy"

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -118,6 +118,7 @@ namespace Onboarding.Areas.Identity.Pages.Account
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
+                await _userManager.AddToRoleAsync(user, "Nowy");
 
                 if (result.Succeeded)
                 {
