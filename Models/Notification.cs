4859e86dc89bc783a2c07101e5c9bfbd3838472e
Sebastian <86176@student.pb.edu.pl>
2025-05-06 15:09:36 +0200

    Added notification model

---
+++ Models/Notification.cs
@@ -0,0 +1,13 @@
+﻿namespace Onboarding.Models
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Message { get; set; }
+        public bool IsRead { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
