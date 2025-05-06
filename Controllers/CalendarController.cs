0a0edc7f95ab61dc6479063ead6edb6f26185e6a
Kamila Szymanowska <86179@student.pb.edu.pl>
2025-05-06 13:44:20 +0200

    FullCalendar Init

---
+++ Controllers/CalendarController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Onboarding.Controllers
+{
+    public class CalendarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetEvents()
+        {
+            // Na razie zwracamy przykładowe dane
+            var events = new[]
+            {
+            new {
+                title = "Spotkanie zespołu",
+                start = "2025-05-10T10:00:00",
+                end = "2025-05-10T11:00:00"
+            },
+            new {
+                title = "Lunch z klientem",
+                start = "2025-05-12T13:00:00",
+                end = "2025-05-12T14:00:00"
+            }
+        };
+
+            return Json(events);
+        }
+    }
+}
