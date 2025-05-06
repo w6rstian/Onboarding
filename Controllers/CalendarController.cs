0e8eebfd3b8751676bb5eb4138465a12411919c4
Kamila Szymanowska <86179@student.pb.edu.pl>
2025-05-06 14:26:38 +0200

    planowanie spotkania z nowym

--- Controllers/CalendarController.cs
+++ Controllers/CalendarController.cs
@@ -1,18 +1,87 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Onboarding.Data;
+using Onboarding.Models;
+using Onboarding.ViewModels;
 
 namespace Onboarding.Controllers
 {
     public class CalendarController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<User> _userManager;
+
+        public CalendarController(ApplicationDbContext context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
         [HttpGet]
+        public async Task<IActionResult> CreateCheckIn()
+        {
+            var currentUser = await _userManager.GetUserAsync(User);
+            var newUsers = await _context.Users
+                .Where(u => u.BuddyId == currentUser.Id)
+                .ToListAsync();
+
+            var model = new CheckInMeetingViewModel
+            {
+                NewUsers = newUsers.Select(u => new SelectListItem
+                {
+                    Value = u.Id.ToString(),
+                    Text = $"{u.Name} {u.Surname} ({u.Email})"
+                }).ToList()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCheckIn(CheckInMeetingViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var currentUser = await _userManager.GetUserAsync(User);
+                model.NewUsers = await _context.Users
+                    .Where(u => u.BuddyId == currentUser.Id)
+                    .Select(u => new SelectListItem
+                    {
+                        Value = u.Id.ToString(),
+                        Text = $"{u.Name} {u.Surname} ({u.Email})"
+                    }).ToListAsync();
+
+                return View(model);
+            }
+
+            var buddy = await _userManager.GetUserAsync(User);
+
+            var meeting = new CheckInMeeting
+            {
+                BuddyId = buddy.Id,
+                NewUserId = model.NewUserId,
+                Start = model.Start,
+                End = model.End,
+                Title = model.Title ?? "Check-In"
+            };
+
+            _context.Add(meeting);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index"); // kalendarz
+        }
+
+        [HttpGet]
         public JsonResult GetEvents()
         {
-            // Na razie zwracamy przykładowe dane
+            // przykładowe dane
             var events = new[]
             {
             new {
---
+++ Models/CheckInMeeting.cs
@@ -0,0 +1,17 @@
+﻿namespace Onboarding.Models
+{
+    public class CheckInMeeting
+    {
+        public int Id { get; set; }
+        public int BuddyId { get; set; }
+        //public User Buddy { get; set; }
+
+        public int NewUserId { get; set; }
+        public User NewUser { get; set; }
+
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+
+        public string? Title { get; set; }
+    }
+}
