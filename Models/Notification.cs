e00b6dd615139160fd65b225e51e6862ed2d7deb
Sebastian <86176@student.pb.edu.pl>
2025-05-27 17:21:18 +0200

    Defined default values

--- Models/Notification.cs
+++ Models/Notification.cs
@@ -5,8 +5,8 @@
         public int Id { get; set; }
         public int UserId { get; set; }
         public string Message { get; set; }
-        public bool IsRead { get; set; }
-        public DateTime CreatedAt { get; set; }
+        public bool IsRead { get; set; } = false;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
 
         public virtual User User { get; set; }
     }
